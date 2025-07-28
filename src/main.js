04de39dedd15343c541589e438232ae0d10481e1
thanin66 <thanin.ra.66@ubu.ac.th>
2025-07-29 01:23:01 +0700

    vue add vuetify

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8000' // URL ของ FastAPI
 
 createApp(App).mount('#app')
